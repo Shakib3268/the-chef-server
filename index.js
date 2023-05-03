1aeff0c1107ec78a45a845008107c21d2919b927
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-05-04 01:32:35 +0600

    dataload

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
+
+const chef = require('./data/chef.json')
+
+app.use(cors())
 
 app.get('/',(req,res) =>{
     res.send('chef client is running')
+})
+
+app.get('/chef',(req,res) =>{
+    res.send(chef)
+})
+
+app.get('/chef/:id',(req,res) => {
+    const id = parseInt(req.params.id )
+    console.log(id)
+    const chefData = chef.find(n => parseInt(n.id) === id)
+    res.send(chefData)
+    console.log(chefData)
 })
 
 app.listen(port,() =>{
